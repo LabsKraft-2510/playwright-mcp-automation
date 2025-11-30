68bc5a97dd103a432e7d6d2c2b26715c72ad3be2
zadmin <zadmin@vm-lab144-16-512.asia-south1-a.c.niit-stackroute-2.internal>
2025-12-01 03:54:30 +0530

    fix(ci): use reporter tuple format in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    'html',
+    ['html'],
     ['./reporters/slack-reporter.ts', { slackWebhookUrl: process.env.SLACK_WEBHOOK, projectName: process.env.npm_package_name }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
